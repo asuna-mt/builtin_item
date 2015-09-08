8649a3a6de2c0e39b8a78ba35e09057d533cb4fd
TenPlus1 <kinsellaja@yahoo.com>
2015-09-08 15:22:17 +0100

    Tweaked and tidied code

--- init_old.lua
+++ init_old.lua
@@ -1,21 +1,24 @@
--- Minetest: builtin/item_entity.lua (8th September 2015)
+-- Minetest: builtin/item_entity.lua (5th July 2015)
 
 function core.spawn_item(pos, item)
-	-- take item in any format
+	-- Take item in any format
 	local stack = ItemStack(item)
 	local obj = core.add_entity(pos, "__builtin:item")
 	obj:get_luaentity():set_item(stack:to_string())
 	return obj
 end
 
--- if item_entity_ttl is not set, enity will have default life time
--- or setting to -1 disables the feature
-local time_to_live = tonumber(core.setting_get("item_entity_ttl")) or 900
+-- If item_entity_ttl is not set, enity will have default life time 
+-- Setting it to -1 disables the feature
 
--- if destroy_item is 1 then dropped items will burn inside lava
+local time_to_live = tonumber(core.setting_get("item_entity_ttl")) or 180 -- 3 mins
+
+-- If destroy_item is 1 then dropped items will burn inside lava
+
 local destroy_item = tonumber(core.setting_get("destroy_item")) or 1
 
--- particle effects for when item is destroyed
+-- Particle effects when item removed
+
 local function add_effects(pos)
 	minetest.add_particlespawner({
 		amount = 1,
@@ -61,7 +64,7 @@ core.register_entity(":__builtin:item", {
 		is_visible = false,
 	},
 
-	itemstring = "",
+	itemstring = '',
 	physical_state = true,
 	age = 0,
 
@@ -77,7 +80,16 @@ core.register_entity(":__builtin:item", {
 		local s = 0.2 + 0.1 * (count / max_count)
 		local c = s
 		local itemtable = stack:to_table()
-		local itemname = itemtable and itemtable.name
+		local itemname = nil
+		if itemtable then
+			itemname = stack:to_table().name
+		end
+		local item_texture = nil
+		local item_type = ""
+		if core.registered_items[itemname] then
+			item_texture = core.registered_items[itemname].inventory_image
+			item_type = core.registered_items[itemname].type
+		end
 		local prop = {
 			is_visible = true,
 			visual = "wielditem",
@@ -92,19 +104,18 @@ core.register_entity(":__builtin:item", {
 	get_staticdata = function(self)
 		return core.serialize({
 			itemstring = self.itemstring,
+			always_collect = self.always_collect,
 			age = self.age
 		})
 	end,
 
 	on_activate = function(self, staticdata, dtime_s)
-
-		-- special function to fast remove entities (xanadu only)
-		if mobs and mobs.entity == false then self.object:remove() end
-
+if mobs and mobs.entity == false then self.object:remove() end
 		if string.sub(staticdata, 1, string.len("return")) == "return" then
 			local data = core.deserialize(staticdata)
 			if data and type(data) == "table" then
 				self.itemstring = data.itemstring
+				self.always_collect = data.always_collect
 				if data.age then 
 					self.age = data.age + dtime_s
 				else
@@ -130,7 +141,7 @@ core.register_entity(":__builtin:item", {
 				overflow = true
 				count = count - max_count
 			else
-				self.itemstring = ""
+				self.itemstring = ''
 			end
 			local pos = object:getpos()
 			pos.y = pos.y + (count - stack:get_count()) / max_count * 0.15
@@ -147,7 +158,8 @@ core.register_entity(":__builtin:item", {
 					collisionbox = {-c, -c, -c, c, c, c}
 				})
 				self.object:remove()
-				return true -- merging succeeded
+				-- merging succeeded
+				return true
 			else
 				s = 0.4
 				c = 0.3
@@ -165,25 +177,26 @@ core.register_entity(":__builtin:item", {
 				self.itemstring = name .. " " .. count
 			end
 		end
-		return false -- merging didn't succeed
+		-- merging didn't succeed
+		return false
 	end,
 
 	on_step = function(self, dtime)
 		self.age = self.age + dtime
-		local p = self.object:getpos()
-
-		-- remove item if old enough or outside map limits
 		if (time_to_live > 0 and self.age > time_to_live)
-		or not within_limits(p) then
-			self.itemstring = ""
+		or not within_limits(self.object:getpos()) then
+			self.itemstring = ''
+			add_effects(self.object:getpos())
 			self.object:remove()
 			return
 		end
 
+		local p = self.object:getpos()
 		p.y = p.y - 0.5
 		local node = core.get_node_or_nil(p)
-		if not node then
-			-- don't infinetly fall into unloaded map
+		local in_unloaded = (node == nil)
+		if in_unloaded then
+			-- Don't infinetly fall into unloaded map
 			self.object:setvelocity({x = 0, y = 0, z = 0})
 			self.object:setacceleration({x = 0, y = 0, z = 0})
 			self.physical_state = false
@@ -192,7 +205,7 @@ core.register_entity(":__builtin:item", {
 		end
 		local nn = node.name
 
-		-- destroy item when dropped into lava (if enabled)
+		-- If item drops into lava then destroy if enabled
 		if destroy_item > 0 and minetest.get_item_group(nn, "lava") > 0 then
 			minetest.sound_play("builtin_item_lava", {
 				pos = p,
@@ -204,8 +217,10 @@ core.register_entity(":__builtin:item", {
 			return
 		end
 
-		-- flowing water pushes item along
-		local nod = minetest.get_node_or_nil({x = p.x, y = p.y + 0.5, z = p.z})
+		p.y = p.y + 0.5
+
+		-- Flowing water pushes item along
+		local nod = minetest.get_node_or_nil(p)
 		if nod and minetest.registered_nodes[nod.name].liquidtype == "flowing" then
 
 			local pos = self.object:getpos()
@@ -243,13 +258,16 @@ core.register_entity(":__builtin:item", {
 			return
 		end
 
-		-- if node is not registered or walkably solid
-		local v = self.object:getvelocity()
-		if not core.registered_nodes[nn] or core.registered_nodes[nn].walkable and v.y == 0 then
+		p.y = p.y - 0.5
+
+		-- If node is not registered or node is walkably solid and resting on nodebox
+		if not core.registered_nodes[nn]
+		or core.registered_nodes[nn].walkable
+		and self.object:getvelocity().y == 0 then
 			if self.physical_state then
 				local own_stack = ItemStack(self.object:get_luaentity().itemstring)
-				-- merge with close entities of the same item
-				for _, object in ipairs(core.get_objects_inside_radius(p, 0.8)) do
+				-- Merge with close entities of the same item
+				for _, object in ipairs(core.get_objects_inside_radius(p, 1.0)) do
 					local obj = object:get_luaentity()
 					if obj and obj.name == "__builtin:item"
 							and obj.physical_state == false then
@@ -273,15 +291,15 @@ core.register_entity(":__builtin:item", {
 		end
 	end,
 
-	on_punch = function(self, puncher)
-		if self.itemstring ~= "" then
-			local left = puncher:get_inventory():add_item("main", self.itemstring)
+	on_punch = function(self, hitter)
+		if self.itemstring ~= '' then
+			local left = hitter:get_inventory():add_item("main", self.itemstring)
 			if not left:is_empty() then
 				self.itemstring = left:to_string()
 				return
 			end
 		end
-		self.itemstring = ""
+		self.itemstring = ''
 		self.object:remove()
 	end,
 })
