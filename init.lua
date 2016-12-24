71108c9b344def1c761d78b8843aca30d3a0329d
TenPlus1 <kinsellaja@yahoo.com>
2016-12-24 13:24:36 +0000

    tidied code (experimental)

--- init.lua
+++ init.lua
@@ -376,6 +376,7 @@ core.register_entity(":__builtin:item", {
 			return
 		end
 
+		-- if inside flowing water then push item
 		if def.liquidtype == "flowing" then
 
 			local vec = quick_flow(pos, node)
@@ -391,6 +392,7 @@ core.register_entity(":__builtin:item", {
 			return
 		else
 
+			-- if on ice then keep on sliding
 			if self.object:getvelocity().y == 0
 			and minetest.get_node({
 				x = pos.x,
@@ -411,67 +413,33 @@ core.register_entity(":__builtin:item", {
 				self.object:setvelocity({x = 0, y = 0, z = 0})
 			end
 
-		-- Collect the items around to merge with
-		local own_stack = ItemStack(self.itemstring)
-		if own_stack:get_free_space() == 0 then
-			return
-		end
+			-- Collect the items around to merge with
+			local own_stack = ItemStack(self.itemstring)
+			if own_stack:get_free_space() == 0 then
+				return
+			end
 
-		local objects = core.get_objects_inside_radius(pos, 0.8)
+			local objects = core.get_objects_inside_radius(pos, 0.8)
 
-		for k, object in pairs(objects) do
+			for k, object in pairs(objects) do
 
-			local entity = object:get_luaentity()
+				local entity = object:get_luaentity()
 
-			if entity and entity.name == "__builtin:item" then
+				if entity and entity.name == "__builtin:item" then
 
-				if self:try_merge_with(own_stack, object, entity) then
+					if self:try_merge_with(own_stack, object, entity) then
 
-					own_stack = ItemStack(self.itemstring)
+						own_stack = ItemStack(self.itemstring)
 
-					if own_stack:get_free_space() == 0 then
-						return
+						if own_stack:get_free_space() == 0 then
+							return
+						end
 					end
 				end
 			end
-		end
 
 			return
 		end
---[[
-print ("---- kerge")
-		self.object:setvelocity({x = 0, y = 0, z = 0})
-		self.physical_state = entity_fall
-		self.object:set_properties({
-			physical = entity_fall
-		})
-
-		self:update_gravity()]]
---[[
-		-- Collect the items around to merge with
-		local own_stack = ItemStack(self.itemstring)
-		if own_stack:get_free_space() == 0 then
-			return
-		end
-
-		local objects = core.get_objects_inside_radius(pos, 1.0)
-
-		for k, object in pairs(objects) do
-
-			local entity = object:get_luaentity()
-
-			if entity and entity.name == "__builtin:item" then
-
-				if self:try_merge_with(own_stack, object, entity) then
-
-					own_stack = ItemStack(self.itemstring)
-
-					if own_stack:get_free_space() == 0 then
-						return
-					end
-				end
-			end
-		end]]
 	end,
 
 	on_punch = function(self, puncher)
